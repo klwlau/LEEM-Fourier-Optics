ffbbf439fbd974178ec8e7ec0a964c5ca9639ffc
Wilson.L <klwilsonlau@gmail.com>
2018-08-02 15:02:17 +0800

    add plotArray func

---
+++ utilityFunc.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plotArray(plot_data, plotSensitivity=3):
+    m, s = np.mean(plot_data), np.std(plot_data)
+    plt.imshow(plot_data, interpolation='nearest', cmap='jet',
+               vmin=m - plotSensitivity * s, vmax=m + plotSensitivity * s,
+               origin='lower')
+    plt.colorbar()
+    plt.show()
