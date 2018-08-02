48428234bf5ccb2e7d144ec2a75de836f42176e3
Wilson.L <klwilsonlau@gmail.com>
2018-08-02 18:49:31 +0800

    fix ^ bug

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from transmittionCrossCoefficientMatrix import *
 print("Start Main")
 
 
+test1 = np.arange(1,10)
+
+xx,yy = np.meshgrid(test1,test1)
+
 
 print(T_o.shape)
 
--- transmittionCrossCoefficientMatrix.py
+++ transmittionCrossCoefficientMatrix.py
@@ -14,10 +14,12 @@ Qj = Qx_j + 1j * Qy_j
 abs_Qi = (Qx_i ** 2 + Qy_i ** 2) ** 0.5
 abs_Qj = (Qx_j ** 2 + Qy_j ** 2) ** 0.5
 
-T_o = np.exp(1j * 2 * np.pi * (1 / 4 * C_3 * lamda ^ 3 * (abs_Qi ** 4 - abs_Qj ** 4)
-                               + 1 / 6 * C_5 * lamda ^ 5 * (abs_Qi ** 6 - abs_Qj ** 6)
+T_o = np.exp(1j * 2 * np.pi * (1 / 4 * C_3 * lamda ** 3 * (abs_Qi ** 4 - abs_Qj ** 4)
+                               + 1 / 6 * C_5 * lamda ** 5 * (abs_Qi ** 6 - abs_Qj ** 6)
                                - 1 / 2 * delta_z * lamda * (abs_Qi ** 2 - abs_Qj ** 2)
                                ))
+
+
 #
 # E_s = np.exp(-np.pi ^ 2 / 4 / log(2) * q_ill ^ 2 *
 # abs(C_3 * lamda ^ 3 * (Qi. * abs_Qi** 2 - Qj. * abs_Qj** 2)
