bb7c000ce1a5eb245e1cec2004fe05dd35998e02
Wilson.L <klwilsonlau@gmail.com>
2018-08-02 19:41:09 +0800

    test speed

--- transmittionCrossCoefficientMatrix.py
+++ transmittionCrossCoefficientMatrix.py
@@ -5,9 +5,9 @@ print("runing transmittionCrossCoefficientMatrix.py")
 maskedQSpaceXX = maskedQSpaceXX[251 - 72:251 + 72, 251 - 72:251 + 72]
 maskedQSpaceYY = maskedQSpaceYY[251 - 72:251 + 72, 251 - 72:251 + 72]
 
-Qx_i, Qx_j = np.meshgrid(maskedQSpaceXX, maskedQSpaceXX, sparse=True)
-Qy_i, Qy_j = np.meshgrid(maskedQSpaceYY, maskedQSpaceYY, sparse=True)
-F_i, F_j = np.meshgrid(maskedWaveObjectFT, maskedWaveObjectFT, sparse=True)
+Qx_i, Qx_j = np.meshgrid(maskedQSpaceXX, maskedQSpaceXX) #, sparse=True)
+Qy_i, Qy_j = np.meshgrid(maskedQSpaceYY, maskedQSpaceYY) #, sparse=True)
+F_i, F_j = np.meshgrid(maskedWaveObjectFT, maskedWaveObjectFT) #, sparse=True)
 
 Qi = Qx_i + 1j * Qy_i
 Qj = Qx_j + 1j * Qy_j
@@ -36,12 +36,12 @@ E_s = np.exp(-np.pi ** 2 / 4 / np.log(2) * q_ill ** 2 *
 
 print("calc E_cc")
 E_cc = (1 - 1j * np.pi / 4 / np.log(2) *
-       delta_fcc * lamda * (abs_Qi_2 - abs_Qj_2)) ** -0.5
+        delta_fcc * lamda * (abs_Qi_2 - abs_Qj_2)) ** -0.5
 
-# E_ct = E_cc. *
-# np.exp(-np.pi ^ 2 / 16 / log(2) *
-# (delta_fc * lamda * (abs_Qi** 2 - abs_Qj** 2)
-#  + 1 / 2 * delta_f3c * lamda ^ 3 * (abs_Qi** 4 - abs_Qj** 4))** 2
-#     . * E_cc** 2)
+# print("calc E_ct")
+# E_ct = E_cc * np.exp(-np.pi ** 2 / 16 / np.log(2) *
+#                      (delta_fc * lamda * (abs_Qi_2 - abs_Qj_2)
+#                       + 1 / 2 * delta_f3c * lamda ^ 3 *
+#                       (abs_Qi_4 - abs_Qj_4)) ** 2 * E_cc ** 2)
 #
 # T = T_o. * E_s. * E_ct
