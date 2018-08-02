c6552919b5bfe0aa270567aa0866521653f9a240
Wilson.L <klwilsonlau@gmail.com>
2018-08-02 19:30:52 +0800

    calc E_cc

--- transmittionCrossCoefficientMatrix.py
+++ transmittionCrossCoefficientMatrix.py
@@ -22,22 +22,21 @@ abs_Qj_2 = abs_Qj ** 2
 abs_Qj_4 = abs_Qj_2 ** 2
 abs_Qj_6 = abs_Qj_2 ** 3
 
-print("calc T_o")
-T_o = np.exp(1j * 2 * np.pi * (1 / 4 * C_3 * lamda ** 3 * (abs_Qi_4 - abs_Qj_4)
-                               + 1 / 6 * C_5 * lamda ** 5 * (abs_Qi_6 - abs_Qj_6)
-                               - 1 / 2 * delta_z * lamda * (abs_Qi_2 - abs_Qj_2)
-                               ))
-print("calc E_s")
-E_s = np.exp(-np.pi** 2 / 4 / np.log(2) * q_ill ** 2 *
-             np.abs(C_3 * lamda ** 3 * (Qi * abs_Qi_2 - Qj * abs_Qj_2)
-                    + C_5 * lamda ** 5 * (Qi * abs_Qi_4 - Qj * abs_Qj_4)
-                    - delta_z * lamda * (Qi - Qj)) ** 2
-             )
-#
-# E_cc = (1 - 1i * np.pi / 4 / log(2) *
-# delta_fcc * lamda * (abs_Qi** 2 - abs_Qj** 2)
-# )** -0.5
-#
+# print("calc T_o")
+# T_o = np.exp(1j * 2 * np.pi * (1 / 4 * C_3 * lamda ** 3 * (abs_Qi_4 - abs_Qj_4)
+#                                + 1 / 6 * C_5 * lamda ** 5 * (abs_Qi_6 - abs_Qj_6)
+#                                - 1 / 2 * delta_z * lamda * (abs_Qi_2 - abs_Qj_2)
+#                                ))
+# print("calc E_s")
+# E_s = np.exp(-np.pi ** 2 / 4 / np.log(2) * q_ill ** 2 *
+#              np.abs(C_3 * lamda ** 3 * (Qi * abs_Qi_2 - Qj * abs_Qj_2)
+#                     + C_5 * lamda ** 5 * (Qi * abs_Qi_4 - Qj * abs_Qj_4)
+#                     - delta_z * lamda * (Qi - Qj)) ** 2)
+
+print("calc E_cc")
+E_cc = (1 - 1j * np.pi / 4 / np.log(2) *
+       delta_fcc * lamda * (abs_Qi ** 2 - abs_Qj ** 2)) ** -0.5
+
 # E_ct = E_cc. *
 # np.exp(-np.pi ^ 2 / 16 / log(2) *
 # (delta_fc * lamda * (abs_Qi** 2 - abs_Qj** 2)
