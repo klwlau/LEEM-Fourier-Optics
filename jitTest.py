26062070f06925bb005c4f45d9632e4f01fe7d79
Wilson.L <klwilsonlau@gmail.com>
2018-08-02 15:03:29 +0800

    fix jit func

--- jitTest.py
+++ jitTest.py
@@ -8,7 +8,7 @@ from numba import vectorize, cuda,jit
 # @vectorize(['float32(float32, float32)'], target='cuda')
 @jit(nopython = True, parallel = True, nogil = True)
 def VectorAdd(a, b):
-    c= a+2*b
+    c= a+b
     return c
 
 
