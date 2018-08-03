3466790cfefd20fdefda241f799718b1e9aa52d5
Wilson.L <klwilsonlau@gmail.com>
2018-08-03 17:36:12 +0800

    rearrange code

--- calMatrixI.py
+++ calMatrixI.py
@@ -9,6 +9,6 @@ EXP = np.exp(1j*2*np.pi*((qq_i - qq_j[:,np.newaxis]).real * maskedQSpaceXX
 abs_maskedWaveObjectFT = (maskedWaveObjectFT * np.conj(maskedWaveObjectFT[:, np.newaxis])).ravel()
 
 
-
-
+def calI(element):
+    return element * T * EXP
 
