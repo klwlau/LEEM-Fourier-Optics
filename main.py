e76db329b4d39ced3c8a48e0766072fb74537511
Wilson.L <klwilsonlau@gmail.com>
2018-08-03 17:20:02 +0800

    debug test

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ print("Start Main")
 num_cores = multiprocessing.cpu_count()
 
 multicoreResults = Parallel(n_jobs=num_cores)(delayed(calI)(element) for element in abs_maskedWaveObjectFT)
+
 for result in multicoreResults:
     result += result
 
-matrixI = np.fft.fftshift(result)
+result = np.fft.fftshift(result)
 
 print("End Main")
