825b630833df6fc9240232174e72fb85080ec789
Wilson.L <klwilsonlau@gmail.com>
2018-08-06 13:45:54 +0800

    replace EXP cal maskedQSpaceXX with sampleCoorRealSpaceXX

--- main.py
+++ main.py
@@ -98,8 +98,8 @@ def calI(element):
 qq_i = maskedQSpaceXX + maskedQSpaceYY*1j
 qq_j = maskedQSpaceXX + maskedQSpaceYY*1j
 
-EXP = np.exp(1j*2*np.pi*((qq_i - qq_j[:,np.newaxis]).real * maskedQSpaceXX
-                         +(qq_i - qq_j[:,np.newaxis]).imag * maskedQSpaceYY ))
+EXP = np.exp(1j*2*np.pi*((qq_i - qq_j[:,np.newaxis]).real * sampleCoorRealSpaceXX
+                         +(qq_i - qq_j[:,np.newaxis]).imag * sampleCoorRealSpaceYY ))
 
 abs_maskedWaveObjectFT = (maskedWaveObjectFT * np.conj(maskedWaveObjectFT[:, np.newaxis])).ravel()
 
