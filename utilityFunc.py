24f61fb8b3e65a7f2627810282b3f6cd0b029c63
Wilson <klwilsonlau@gmail.com>
2018-08-08 21:45:47 +0800

    update plot

--- utilityFunc.py
+++ utilityFunc.py
@@ -4,7 +4,7 @@ import numpy as np
 def plotArray(plot_data, plotSensitivity=1):
     m, s = np.mean(plot_data), np.std(plot_data)
     plt.imshow(plot_data, interpolation='nearest', cmap='jet',
-               vmin=m - plotSensitivity * s, vmax=m + plotSensitivity * s,
+               vmin=0, vmax=2,
                origin='lower')
     plt.colorbar()
     plt.show()
