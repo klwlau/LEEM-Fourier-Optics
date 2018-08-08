dd1f26dc786c3b36e52507e110af0e7836e27b47
Wilson.L <klwilsonlau@gmail.com>
2018-08-08 10:55:20 +0800

    test wraper

--- wraper.py
+++ wraper.py
@@ -1,5 +1,13 @@
 from main import *
 
 
+alpha_apList = np.linspace(0.5E-3, 1E-3, num=10)
+print(alpha_apList)
 
-main()
+loopMainCounter = 0
+loopLen = len(alpha_apList)
+for alp_size in alpha_apList:
+    loopMainCounter += 1
+    print("alpha_ap:", alp_size, loopMainCounter,"/",loopLen)
+    alpha_ap = alp_size
+    main()
