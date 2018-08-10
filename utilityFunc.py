a8b3ca85381be8880045d391aba1b8618c244a05
Wilson.L <klwilsonlau@gmail.com>
2018-08-10 11:14:02 +0800

    update

--- utilityFunc.py
+++ utilityFunc.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def plotArray(plot_data, plotSensitivity=1):
-    m, s = np.mean(plot_data), np.std(plot_data)
     plt.imshow(plot_data, interpolation='nearest', cmap='jet',
                vmin=0, vmax=2,
                origin='lower')
--- wraper.py
+++ wraper.py
@@ -2,19 +2,19 @@ from datetime import timedelta
 from main import *
 wraperStartTime = time.time()
 
-# from utilityFunc import *
-# matrixI = main()
-# plotArray(matrixI)
+from utilityFunc import *
+matrixI = main(alpha_ap)
+plotArray(matrixI)
 
 
-alpha_apList = np.linspace(0.1E-3, 7.5E-3, num=10)
-print(alpha_apList)
-
-loopMainCounter = 0
-loopLen = len(alpha_apList)
-for alp_size in alpha_apList:
-    loopMainCounter += 1
-    print("alpha_ap:", alp_size, loopMainCounter, "/", loopLen)
-    main(alp_size)
+# alpha_apList = np.linspace(0.1E-3, 7.5E-3, num=10)
+# print(alpha_apList)
+#
+# loopMainCounter = 0
+# loopLen = len(alpha_apList)
+# for alp_size in alpha_apList:
+#     loopMainCounter += 1
+#     print("alpha_ap:", alp_size, loopMainCounter, "/", loopLen)
+#     main(alp_size)
 
 print("Total Time: "+str(timedelta(seconds=time.time() - wraperStartTime)))
