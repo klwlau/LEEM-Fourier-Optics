babc663391ec883f9f54c1bc16012f36236605ea
Wilson.L <klwilsonlau@gmail.com>
2018-11-12 13:55:26 +0800

    new NAC-_-object

--- constants.py
+++ constants.py
@@ -14,16 +14,16 @@ objectSpaceSize = 10 * 1e-9  # nm #5
 
 
 
-U_a = 15.01e3  # eV  Accelerating Voltage
+U_a = 20e3  # eV  Accelerating Voltage
 U_o = 10  # eV  Electron Voltage
-delta_E = 0.25  # eV  Energy Spread
+delta_E = 0.75  # eV  Energy Spread
 C_3 = 0.345  # m   Spherical Aberration Coefficient
-C_5 = 39.4
-C_c = -0.075  # m   Chromatic Aberration Coefficient
-C_3c = -58.37
-C_cc = 23.09
-alpha_ap = 2.34E-3  # rad Acceptance Aangle of the Contrast Aperture
-alpha_ill = 0.122E-3  # rad Illumination Divergence Angle
+C_5 = 0
+C_cc = 0
+C_3c = 0
+C_c = -0.106  # m   Chromatic Aberration Coefficient
+alpha_ap = 1.4E-3  # rad Acceptance Aangle of the Contrast Aperture
+alpha_ill = 0.11E-3  # rad Illumination Divergence Angle
 # alpha_ap = 1.37E-3  # rad Acceptance Aangle of the Contrast Aperture
 # alpha_ap = 2.37E-3  # rad Acceptance Aangle of the Contrast Aperture
 # alpha_ap = 7.37E-3  # rad Acceptance Aangle of the Contrast Aperture
--- utilityFunc.py
+++ utilityFunc.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plotArray(plot_data, plotSensitivity=1):
+def plotArray(plot_data, vmin = -1,vmax=1):
     plt.imshow(plot_data.T, interpolation='nearest', cmap='jet', #np.flipud(plot_data)
-               origin='lower',vmin=-1, vmax=1) #vmin=0, vmax=2,
+               origin='lower',vmin=vmin, vmax=vmax) #vmin=0, vmax=2,
     plt.colorbar()
     plt.show()
