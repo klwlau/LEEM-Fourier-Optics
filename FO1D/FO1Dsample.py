261d0ea087ee74a2ec1c6479ea8bb8fa84c6f159
Wilson <klwilsonlau@gmail.com>
2019-09-04 14:08:31 +0800

    update

--- FO1D/FO1Dsample.py
+++ FO1D/FO1Dsample.py
@@ -1,11 +1,13 @@
-from FO1Dconstants import *
+
+import numpy as np
 
 def create2DSimulatedObject():
-    t=100
-    fs=1000
-    samples = np.linspace(0, t, t * fs)
-    simulatedObject = np.sin(samples)
+    object_wavelength = 900e-9
+    n_sample = 1 + 2 ^ 10
+    simulatedObject = np.linspace(-object_wavelength, object_wavelength, n_sample)
 
+    K = [3,4,5,6] * 2
+    n_max = np.floor(q_max / (1 / object_wavelength));
 
     return simulatedObject
 
