2fdc237fa2080083a5af876e0ae6c38c6052498b
Wilson <klwilsonlau@gmail.com>
2019-09-12 10:22:42 +0800

    update constant

--- FO1Dconstants.py
+++ FO1Dconstants.py
@@ -7,7 +7,7 @@ else:
 
 
 
-resultFileNote = "sineObject"
+resultFileNote = "ACsineObject"
 timezone = 'Asia/Hong_Kong'
 numberOfThreads = -1 # number of threads used in the simulation with -1 means using all threads, -2 means using all threads minus 1.
 
@@ -17,15 +17,16 @@ simulatingSpaceSize = 25 * 1e-9  # total simulating space in nm
 
 U_a = 15.01e3  # eV  Accelerating Voltage
 U_o = 10  # eV  Electron Voltage
-delta_E = 0.25  # eV  Energy Spread
-C_3 = 0  # m   Spherical Aberration Coefficient
-C_5 = 92.8
 C_c = 0  # m   Chromatic Aberration Coefficient
 C_3c = -67.4
 C_cc = 27.9
+C_3 = 0  # m   Spherical Aberration Coefficient
+C_5 = 92.8
+
 alpha_ill = 0.1E-3  # rad Illumination Divergence Angle
 alpha_ap = 7.37E-3  # rad Acceptance Aangle of the Contrast Aperture
 
+delta_E = 0.25  # eV  Energy Spread
 M_L = 0.653  # Lateral Magnification
 # defocus = 0  # mA
 # delta_zo = -1 * defocus * 5.23 * 10 ** -6  # m
