f43ec40ba87ac7a5f0ae9367297abbf76a6f2c7c
Wilson <klwilsonlau@gmail.com>
2020-03-07 19:28:56 +0800

    rewrite code

--- CTF1Dconstants.py
+++ CTF1Dconstants.py
@@ -7,45 +7,45 @@ else:
 
 
 
-taskName = "ACSinObject"
+taskName = "NACSinObject"
 timezone = 'Asia/Hong_Kong'
 numberOfThreads = -1 # number of threads used in the simulation with -1 means using all threads, -2 means using all threads minus 1.
 
 simulatingSpaceSize = 25 * 1e-9  # total simulating space in nm
 # simulatingSpaceTotalStep = 501  # total simulating steps
 
-##################### AC Constants######################
-U_a = 10.01e3  # eV  Accelerating Voltage
-U_o = 10  # eV  Electron Voltage
-C_c = 0  # m   Chromatic Aberration Coefficient
-C_3c = -67.4
-C_cc = 27.9
-C_3 = 0  # m   Spherical Aberration Coefficient
-C_5 = 92.8
-
-alpha_ap = 7.37E-3  # rad Acceptance Aangle of the Contrast Aperture
-alpha_ill = 0.1E-3  # rad Illumination Divergence Angle
-
-delta_E = 0.25  # eV  Energy Spread
-M_L = 0.653  # Lateral Magnification
-##################### AC Constants######################
-
-
-# ##################### NAC Constants######################
-# U_a = 15.01e3  # eV  Accelerating Voltage
+# ##################### AC Constants######################
+# U_a = 10.01e3  # eV  Accelerating Voltage
 # U_o = 10  # eV  Electron Voltage
-# C_c = -0.075  # m   Chromatic Aberration Coefficient
-# C_3c = -59.37
-# C_cc = 23.09
-# C_3 = 0.345  # m   Spherical Aberration Coefficient
-# C_5 = 39.4
+# C_c = 0  # m   Chromatic Aberration Coefficient
+# C_3c = -67.4
+# C_cc = 27.9
+# C_3 = 0  # m   Spherical Aberration Coefficient
+# C_5 = 92.8
 #
-# alpha_ap = 2.34E-3  # rad Acceptance Aangle of the Contrast Aperture
+# alpha_ap = 7.37E-3  # rad Acceptance Aangle of the Contrast Aperture
 # alpha_ill = 0.1E-3  # rad Illumination Divergence Angle
 #
 # delta_E = 0.25  # eV  Energy Spread
 # M_L = 0.653  # Lateral Magnification
-# ##################### NAC Constants######################
+# ##################### AC Constants######################
+
+
+##################### NAC Constants######################
+U_a = 15.01e3  # eV  Accelerating Voltage
+U_o = 10  # eV  Electron Voltage
+C_c = -0.075  # m   Chromatic Aberration Coefficient
+C_3c = -59.37
+C_cc = 23.09
+C_3 = 0.345  # m   Spherical Aberration Coefficient
+C_5 = 39.4
+
+alpha_ap = 2.34E-3  # rad Acceptance Aangle of the Contrast Aperture
+alpha_ill = 0.1E-3  # rad Illumination Divergence Angle
+
+delta_E = 0.25  # eV  Energy Spread
+M_L = 0.653  # Lateral Magnification
+##################### NAC Constants######################
 
 
 delta_z = np.linspace(-200E-6,200E-6,101)
